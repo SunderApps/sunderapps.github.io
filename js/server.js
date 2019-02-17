3b6b2ee4f22e399cdd00890804fa2b39747a1a1a
Sam Underwood <sam@sunderapps.com>
2019-02-16 22:30:01 -0500

    Almost

---
+++ js/server.js
@@ -0,0 +1,52 @@
+var $under = $under || {};
+$under.$erver = $under.$erver || {
+    cache: [],
+
+    open: function (index) {
+        $under.$erver.cache[index]
+            .removeClass('hide')
+            .addClass('open');
+        if (index == 1) {
+            if ($under.web && $under.web.init) {
+                $under.web.init();
+            }
+        }
+    },
+
+    load: function (html, index) {
+        $under.$erver.cache[index] = $('<div></div>')
+            .addClass('container')
+            .html(html);
+        
+        $('body').append($under.$erver.cache[index]);
+        $under.$erver.open(index);
+    },
+
+    get: function (file, index) {
+        $.get(file, function (html) {
+            $under.$erver.load(html, index);
+        }, 'html');
+    },
+
+    nav: function () {
+        var index = $(this).index();
+        $('.container.open').addClass('hide')
+        setTimeout(function () { $('.container.open.hide').removeClass('open').removeClass('hide'); }, 300);
+        if ($under.$erver.cache[index]) {
+            $under.$erver.open(index);
+        } else {
+            var file = $(this).data('file');
+            $under.$erver.get(file, index);
+        }
+    },
+
+    events: function () {
+        $('nav ul li').on('click', $under.$erver.nav);
+    },
+
+    init: function () {
+        $under.$erver.events();
+    }
+};
+
+$($under.$erver.init);
---
+++ js/sunder.js
@@ -0,0 +1,61 @@
+var $under = $under || {
+
+    gradient: function (color) {
+        var width = 5;
+        $(window).on("mousemove", function (event) {
+            var center = Math.floor((event.clientX / $(window).width()) * 100);
+            $('.progress').css({ background: 'linear-gradient(to right, #343A40 0%, #343A40 ' + (center - width) + '%, ' + color + ' ' + center + '%, #343A40 ' + (center + width) + '%, #343A40 100%)' });
+        });
+    },
+
+    sidebar: {
+        toggle: function () {
+            $('nav').toggleClass('show')
+        }
+    },
+
+    events: function () {
+        $('nav .toggle').on('click', $under.sidebar.toggle);
+    },
+
+    init: function () {
+        $under.events();
+        $under.gradient('#13771e');
+    }
+};
+
+$($under.init);
+
+// Web
+$under.web = $under.web || {
+
+    resize: function (e) {
+        $.each($('.web-grid iframe'), function (index, iframe) {
+            $(iframe).height($(iframe).width() / 1.77942539388);
+        });
+    },
+
+    update: function (e) {
+        var $this = $(this),
+            checked = $this.is(':checked'),
+            $iframe = $this.parent().siblings('iframe');
+        $('.web-grid iframe').attr('src', '');
+        if (checked) {
+            $iframe.attr('src', $iframe.data('src'));
+        } else {
+            $iframe.attr('src', '');
+        }
+    },
+
+    events: function () {
+        $(window).off('resize').on('resize', $under.web.resize);
+        $('.web-grid input[type="radio"]').off('change').on('change', $under.web.update);
+    },
+
+    init: function () {
+        $under.web.events();
+        $under.web.resize();
+    }
+};
+
+$($under.web.init);
