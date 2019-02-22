a36829964ec476b9a0ffe69f29e48dc281924140
Sam Underwood <sam@sunderapps.com>
2019-02-22 13:12:16 -0500

    History API Test

---
+++ js/history.js
@@ -0,0 +1,28 @@
+var $under = $under || {};
+$under.history = $under.history || {
+    state: {},
+
+    location: function () {
+        return document.location;
+    },
+
+    push: function (title, url) {
+        history.pushState({ page: url }, title, url);
+    },
+
+    pop: function (e) {
+        console.log(e);
+        console.log(e.state);
+        console.log($under.history.location());
+    },
+
+    events: function () {
+        $(window).on('popstate', $under.history.pop);
+    },
+
+    init: function () {
+        $under.history.events();
+    }
+};
+
+$($under.history.init);
