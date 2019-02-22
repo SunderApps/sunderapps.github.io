ee01cb4824140d1abfcd4b780cce4451f876c5fa
Sam Underwood <sam@sunderapps.com>
2019-02-22 17:16:21 -0500

    Server Changes

--- js/server.js
+++ js/server.js
@@ -13,7 +13,15 @@ $under.$erver = $under.$erver || {
         }
     },
 
-    load: function (html, index) {
+    loadScript: function (html) {
+
+    },
+
+    loadHead: function (html) {
+
+    },
+
+    loadContent: function (html, index) {
         $under.$erver.cache[index] = $('<div></div>')
             .addClass('container')
             .html(html);
@@ -22,10 +30,30 @@ $under.$erver = $under.$erver || {
         $under.$erver.open(index);
     },
 
-    get: function (file, index) {
-        $.get(file, function (html) {
-            $under.$erver.load(html, index);
-        }, 'html');
+    get: function (page, index) {
+        var lib = [
+            new Promise((res2, rej2) => {
+                $.get('/views/head/' + page + '.html', function (html) {
+                    $under.$erver.loadHead(html);
+                    res2('head');
+                    console.log('head');
+                }, 'html');    
+            }), 
+            new Promise((res3, rej3) => {
+                $.get('/views/content/' + page + '.html', function (html) {
+                    $under.$erver.loadContent(html, index);
+                    res3('content');
+                    console.log('content');
+                }, 'html');    
+            })
+        ];
+        var con = new Promise((res1, rej1) => {
+            $.get('/views/script/' + page + '.html', function (html) {
+                $under.$erver.loadScript(html);
+                res1('script');
+                console.log('script');
+            }, 'html');    
+        }).then((ret1) => Promise.all(lib));
     },
 
     nav: function () {
@@ -36,8 +64,7 @@ $under.$erver = $under.$erver || {
         if ($under.$erver.cache[index]) {
             $under.$erver.open(index);
         } else {
-            var file = '/views/' + page + '.html';
-            $under.$erver.get(file, index);
+            $under.$erver.get(page, index);
         }
         $under.history.push('Sunder | ' + (page.charAt(0).toUpperCase() + page.slice(1)), '/' + page + '/');
     },
