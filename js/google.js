e69fcb70272ff930566d65cc00505fc1e3c706dc
Sam Underwood <sam@sunderapps.com>
2019-02-23 10:34:00 -0500

    Links

--- js/google.js
+++ js/google.js
@@ -1,6 +1,15 @@
+/*
+References: 
+    https://developers.google.com/gtagjs/reference/event
+*/
+
 var $under = $under || {};
 $under.google = $under.google || {
     trackingId: 'UA-134242899-2',
+
+    exception: function (description, fatal) {
+        gtag('event', 'exception', { 'description': description, 'fatal': fatal });
+    },
 
     setPage: function (url) {
         gtag('config', $under.google.trackingId, { 'page_path': url });
