7472309ec2e15c9721ebc247341d90bfca33b1fe
Sam Underwood <sam@sunderapps.com>
2019-02-24 18:59:37 -0500

    Starting Player

--- js/audio.js
+++ js/audio.js
@@ -2,15 +2,33 @@ var $under = $under || {};
 $under.audio = $under.audio || {
     element: {},
 
+    play:()=>{
+        $under.audio.element.play();
+    },
+
+    pause:()=>{
+        $under.audio.element.pause();
+    },
+
+    toggle:()=>{
+        
+    },
+
     load:(src,title)=>{
         if (src) {
             $under.audio.element.src = src;
+            $under.audio.element.load();
             $('.player .title').text(title);
         }
     },
 
+    events:()=>{
+
+    },
+
     init:()=>{
         $under.audio.element = new Audio();
+        $under.audio.events();
     }
 };
 $($under.audio.init);
