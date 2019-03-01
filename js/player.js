be45a7b31eed46769ba1d5cbffc837f46177e55e
Sam Underwood <sam@sunderapps.com>
2019-02-28 21:48:14 -0500

    Player 1

--- js/player.js
+++ js/player.js
@@ -4,26 +4,51 @@ $under.player = $under.player || {
 
     play:()=>{
         $under.player.element.play();
+        $('.player .fa-pause').addClass('fa-play').removeClass('fa-pause');
     },
 
     pause:()=>{
         $under.player.element.pause();
+        $('.player .fa-play').addClass('fa-pause').removeClass('fa-play');
     },
 
-    toggle:()=>{
-        
+    togglePlay:()=>{
+        if($('.player .play i').hasClass('fa-pause')) {
+            $under.player.play();
+        } else {
+            $under.player.pause();
+        }
+    },
+
+    mute:()=>{
+
+    },
+
+    unmute:()=>{
+
+    },
+
+    toggleMute:()=>{
+
     },
 
     load:(src,title)=>{
         if (src) {
             $under.player.element.src = src;
             $under.player.element.load();
-            $('.player .title').text(title);
+            $('.player .title p').text(title);
         }
     },
 
     events:()=>{
-
+        $('.player .play i').on('click', $under.player.togglePlay);
+        $('.player .play input')
+            .on('change, input', $under.player.setTime)
+            .on('mousedown', $under.player.pause)
+            .on('mouseup', $under.player.play);
+        $('.player .volume i').on('click', $under.player.toggleMute);
+        $('.player .volume input').on('change, input', $under.player.volume);
+        $('.player .full-screen i').on('click', $under.player.fullScreen);
     },
 
     init:()=>{
