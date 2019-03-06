5a8c644a6e276d3d4bd607f03e1debe615f153b8
Sam Underwood <sam@sunderapps.com>
2019-03-05 20:27:31 -0500

    PG Start

---
+++ js/pg.js
@@ -0,0 +1,46 @@
+var $under = $under || {};
+$under.pg = $under.pg || {
+
+    update: function () {
+        var d = new Date(),
+            a = d.getDate(),
+            t = d.getMonth() + 1,
+            e = d.getFullYear();
+        $.getJSON('https://www.statbureau.org/calculate-inflation-rate-json',
+        {
+            country: 'united-states',
+            start: '2006/1/1',
+            end: e + '/' + t + '/' + a,
+            amount: '100',
+            format: true
+        }).done(function (json) {
+            console.log(json);
+        });
+        $.getJSON('https://www.statbureau.org/calculate-inflation-price-json',
+        {
+            country: 'united-states',
+            start: '2006/1/1',
+            end: e + '/' + t + '/' + a,
+            amount: '100',
+            format: true
+        }).done(function (json) {
+            console.log(json);
+        });
+        $.getJSON('https://www.statbureau.org/calculate-inflation-value-json',
+        {
+            country: 'united-states',
+            start: '2006/1/1',
+            end: e + '/' + t + '/' + a,
+            amount: '100',
+            format: true
+        }).done(function (json) {
+            console.log(json);
+        });
+
+    },
+
+    init: function () {
+        $under.pg.update();
+    }
+};
+$($under.pg.init);
