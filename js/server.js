6a4ba17122e7a30a95bbf04fef88afed70e9f2c5
Sam Underwood <sam@sunderapps.com>
2019-03-18 11:54:01 -0400

    Update server.js

--- js/server.js
+++ js/server.js
@@ -77,8 +77,9 @@ $under.$erver = $under.$erver || {
 
     load: function () {
         var page = $under.$erver.getLocation().slice(1, -1),
-            $this = $('nav .links ul li a[href="/' + page + '/"], nav .links ul li a[data-page="' + page + '"]').first(),
+            $this = $('nav .links ul li a[href=/' + page + '/], nav .links ul li a[data-page=' + page + ']').first(),
             index = $this.parent().index();
+            console.log($this);
         console.log(page);
         console.log(index);
         $under.$erver.navigate(page, index);
