af08f667c9aa686e34f50a8bee6b7d0e8e2dd39f
Samuel Underwood <sam@sunderapps.com>
2019-04-17 07:05:43 -0400

    Web Update

--- js/sunder.js
+++ js/sunder.js
@@ -7,6 +7,42 @@ var delay = (function() {
 })();
 
 var $under = $under || {
+	storage: {
+        enabled: false,
+
+        set: function (key, item) {
+            if ($under.storage.enabled) {
+                window.localStorage.setItem(key, item);
+            }
+        },
+
+        get: function (key) {
+            if ($under.storage.enabled) {
+                return window.localStorage.getItem(key);
+            } else {
+                return 'undefined';
+            }
+        },
+
+        remove: function (key) {
+            if ($under.storage.enabled) {
+                window.localStorage.removeItem(key);
+            }
+        },
+
+        clear: function () {
+            if ($under.storage.enabled) {
+                window.localStorage.clear();
+            }
+        },
+
+        init: function () {
+            if (typeof Storage !== 'undefined') {
+                $under.storage.enabled = true;
+            }
+        }
+    },
+	
     sidebar: {
         toggle: function () {
             $('nav').toggleClass('show')
@@ -22,6 +58,7 @@ var $under = $under || {
 
     init: function () {
         $under.events();
+		$under.storage.init();
     }
 };
 
