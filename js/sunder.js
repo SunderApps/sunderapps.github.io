1b6127f166b652da87b575d999a43132097889f1
Sam Underwood <stu17@pitt.edu>
2019-04-18 15:14:12 -0400

    Background JS

--- js/sunder.js
+++ js/sunder.js
@@ -52,6 +52,78 @@ var $under = $under || {
         }
     },
 
+    contact: {
+        sendTo: 'sam@sunderapps.com',
+        send: function (name, subject, message) {
+            $.post(
+                'https://sunder-functions20190319082035.azurewebsites.net/api/SendEmail',
+                {
+                    'name': 'Sam Underwood',
+                    'email': 'sam@sunderapps.com',
+                    'subject': 'Testing',
+                    'message': 'Please work!'
+                }
+            ).done(function (data) {
+                alert('Email sent successfully!');
+            }).fail(function (xhr, status, error) {
+                alert('Sending email failed.  Please manually send your message to ' + $under.contact.sendTo);
+            });
+        },
+
+        init: function () {
+
+        }
+    },
+
+    background: {
+        lastSrc: '',
+        lastPos: '',
+        elements: [],
+        actives: [],
+        update: function () {
+            var $container = $('.container.open:not(.hide)');
+            if ($container) {
+                var index = Math.floor(Math.max($container.scrollTop() + ($(window).height() / 3), 0) / $(window).height()),
+                    $img = $($($container.children()[0]).children()[index]).find('img'),
+                    src = $img.data('background') || $img.attr('src') || $under.background.lastSrc,
+                    pos = $img.data('position') || $under.background.lastPos;
+                $under.background.lastSrc = src;
+                $under.background.lastPos = pos;
+                if ($('body').css('backgroundImage') !== 'url(' + src + ')' && !~$under.background.actives.indexOf(src)) {
+                    $under.background.elements[index] = $under.background.elements[index] || $('<div></div>').addClass('background');    
+                    $under.background.actives[index] = src;
+                    $('body').append($under.background.elements[index]);
+                    $under.background.elements[index].css({
+                        backgroundImage: 'url(' + src + ')',
+                        backgroundPosition: pos
+                    }).animate({
+                        opacity: 1
+                    }, 500, function () {
+                        $('body').css({
+                            backgroundImage: 'url(' + src + ')',
+                            backgroundPosition: pos
+                        });
+                        $under.background.elements[index].remove();
+                        $under.background.elements[index] = null;
+                        $under.background.actives[index] = null;
+                    });
+                }
+            }
+        },
+
+        openPage: function () {
+            $('.container.open:not(.hide)').off('scroll').on('scroll', $under.background.update).scroll();
+        },
+
+        events: function () {
+            $under.$erver.on('$-open-page', $under.background.openPage);
+        },
+        
+        init: function () {
+            $under.background.events();
+        }
+    },
+
     events: function () {
         $('nav .toggle').on('click', $under.sidebar.toggle);
     },
@@ -59,24 +131,8 @@ var $under = $under || {
     init: function () {
         $under.events();
         $under.storage.init();
-        
-        ///* DEBUGGING FOR SUNDER FUNCTIONS
-        $.post(
-            'https://sunder-functions20190319082035.azurewebsites.net/api/SendEmail',
-            {
-                'name': 'Sam Underwood',
-                'email': 'sam@sunderapps.com',
-                'subject': 'Testing',
-                'message': 'Please work!'
-            }
-        ).done(function (data) {
-            console.log('success!');
-            console.log(data);
-        }).fail(function (xhr, status, error) {
-            console.log('error!');
-            console.log(error);
-        });
-        //*/
+        $under.contact.init();
+        $under.background.init();
     }
 };
 
