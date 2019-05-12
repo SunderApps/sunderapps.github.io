c7dd4c963e97fe279048fca6e41185b3b2bf979d
Sam Underwood <stu17@pitt.edu>
2019-05-12 13:49:00 -0400

    Test Code

--- js/sunder.js
+++ js/sunder.js
@@ -82,7 +82,9 @@ var $under = $under || {
         actives: [],
         update: function () {
             var $container = $('.container.open:not(.hide)');
+            console.log('background-update');
             if ($container) {
+                console.log($container);
                 var index = Math.floor(Math.max($container.scrollTop() + ($(window).height() / 3), 0) / $(window).height()),
                     $img = $($($container.children()[0]).children()[index]).find('img'),
                     src = $img.data('background') || $img.attr('src') || $under.background.lastSrc,
